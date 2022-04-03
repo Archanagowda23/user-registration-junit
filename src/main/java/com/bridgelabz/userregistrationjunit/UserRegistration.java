2a92f643dd6c75be449a8c7973061a3e587ae1e5
Archanagowda23 <archanar249@gmail.com>
2022-04-03 12:16:38 +0530

    initial commit

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.userregistrationjunit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * purpose write a junit program to validation of user registration program
+ * Validatin use the regex program
+ * @author : Archana
+ * @since : 03-04-2022
+ */
+
+public class UserRegistration {
+    Pattern pattern;
+    Matcher matcher;
+}
---
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.userregistrationjunit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    UserRegistration userRegistration = new UserRegistration();
+
+}
